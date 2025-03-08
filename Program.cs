ab8731292bb7cacad89af908a9645faca174c068
miguel <mdias1300@gmail.com>
2025-03-07 22:04:52 -0300

    resolucao atividade

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(" Noites traiçoieras");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(@"Deus está aqui neste momento
+Sua presença é real em meu viver
+Entregue sua vida e seus problemas
+Fale com Deus, Ele vai ajudar você
+
+Oh, oh, Deus te trouxe aqui
+Para aliviar os teus sofrimento
+Oh, oh, é Ele o autor da Fé
+Do princípio ao fim
+Em todos os seus tormentos");
+
+Console.WriteLine();
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.WriteLine(@"E ainda se vier noite traiçoeira
+Se a cruz pesada for, Cristo estará contigo
+O mundo pode até fazer você chorar
+Mas Deus te quer sorrindo
+
+E ainda se vier noite traiçoeira
+Se a cruz pesada for, Cristo estará contigo
+O mundo pode até fazer você chorar
+Mas Deus te quer sorrindo");
+
+Console.ResetColor();
